7258a70f458c0d1c76d7107da082639bd0304110
azizkale <azizkale@hotmail.com>
2024-09-05 11:11:18 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.send("Hello K8s");
+});
+
+app.get("/secret", (req, res) => {
+  res.send("my secret: " + process.env.MY_SECRET);
+});
+
+app.get("/pipeline", (req, res) => {
+  res.send("pipeline works!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
